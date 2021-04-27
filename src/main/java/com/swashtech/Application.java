adfb9e647b6ad47fbca9c1732d5ec8f28beac8ef
Ashish Sirsikar <asirsikar319@gmail.com>
2021-04-27 09:26:42 +0530

    changes for docker

--- src/main/java/com/swashtech/Application.java
+++ src/main/java/com/swashtech/Application.java
@@ -2,13 +2,20 @@ package com.swashtech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@PropertySource("file:./etc/config/application.properties")
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		System.err.print("Starting SpringBoot");
+		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+		ConfigurableEnvironment env = context.getEnvironment();
+		System.err.println("MongoDB URL -------> : "+env.getProperty("spring.data.mongodb.uri"));
 	}
 }
--- src/main/java/com/swashtech/config/SwaggerConfig.java
+++ src/main/java/com/swashtech/config/SwaggerConfig.java
@@ -1,7 +1,12 @@
 package com.swashtech.config;
 
+import static com.google.common.base.Predicates.or;
+import static springfox.documentation.builders.PathSelectors.regex;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.FileSystemResource;
 
 import com.google.common.base.Predicate;
 
@@ -10,8 +15,6 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import static springfox.documentation.builders.PathSelectors.regex;
-import static com.google.common.base.Predicates.or;
 
 @Configuration
 @EnableSwagger2
@@ -19,8 +22,7 @@ public class SwaggerConfig {
 
 	@Bean
 	public Docket postsApi() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.groupName("swashtech-api").apiInfo(apiInfo()).select()
+		return new Docket(DocumentationType.SWAGGER_2).groupName("swashtech-api").apiInfo(apiInfo()).select()
 				.paths(postPaths()).build();
 	}
 
