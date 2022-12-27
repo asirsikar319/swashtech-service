baeb37592f771bc443b0d01532260735203fd2a6
Ashish Sirsikar <asirsikar319@gmail.com>
2022-12-27 09:37:56 +0530

    Update pom.xml

--- src/main/java/com/swashtech/Application.java
+++ src/main/java/com/swashtech/Application.java
@@ -3,6 +3,7 @@ package com.swashtech;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.ConfigurableEnvironment;
 
@@ -16,4 +17,5 @@ public class Application {
 		ConfigurableEnvironment env = context.getEnvironment();
 		System.err.println("MongoDB URL -------> : "+env.getProperty("spring.data.mongodb.uri"));
 	}
+	
 }
